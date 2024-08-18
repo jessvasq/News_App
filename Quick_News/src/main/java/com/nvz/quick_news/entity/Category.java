0f15d9fcb621b8642a41b11079df4022afdad69f
jessvasq <jessnsq.codes@gmail.com>
2024-08-18 00:15:37 -0400

    Category entity

--- Quick_News/src/main/java/com/nvz/quick_news/entity/Category.java
+++ Quick_News/src/main/java/com/nvz/quick_news/entity/Category.java
@@ -1,4 +1,30 @@
 package com.nvz.quick_news.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String description;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    // Relationships
+    @OneToMany
+    private List<Article> articles;
 }
