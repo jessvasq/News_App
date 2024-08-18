de3f890894e2ff091477da5551af1a74321d34df
jessvasq <jessnsq.codes@gmail.com>
2024-08-17 23:02:14 -0400

    project setup & file structure

---
+++ Quick_News/src/main/java/com/nvz/quick_news/QuickNewsApplication.java
@@ -0,0 +1,13 @@
+package com.nvz.quick_news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickNewsApplication.class, args);
+    }
+
+}
