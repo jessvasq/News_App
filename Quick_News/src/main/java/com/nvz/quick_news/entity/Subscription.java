ba279671cf89d896bdc9ddba01bdf03a5003219c
jessvasq <jessnsq.codes@gmail.com>
2024-08-18 17:49:35 -0400

    subscription entity

--- Quick_News/src/main/java/com/nvz/quick_news/entity/Subscription.java
+++ Quick_News/src/main/java/com/nvz/quick_news/entity/Subscription.java
@@ -1,4 +1,25 @@
 package com.nvz.quick_news.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name="subscription")
 public class Subscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long subscriptionId;
+    private LocalDateTime createdAt;
+
+    // Relationships
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable=false)
+    private User user;
 }
